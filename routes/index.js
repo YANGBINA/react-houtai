bfceef6e850664b66f8b5ff26d9ef780d16f4960
YANGBINA <1539068922@qq.com>
2018-11-08 13:32:08 +0800

    'asd'

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,29 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.post('/api/login/account',(req, res) => {
+  const { password, userName, type } = req.body;
+  if (password === 'ant.design' && userName === 'admin') {
+    res.send({
+      status: 'ok',
+      type,
+      currentAuthority: 'admin',
+    });
+    return;
+  }
+  if (password === 'ant.design' && userName === 'user') {
+    res.send({
+      status: 'ok',
+      type,
+      currentAuthority: 'user',
+    });
+    return;
+  }
+  res.send({
+    status: 'error',
+    type,
+    currentAuthority: 'guest',
+  });
+})
 
 module.exports = router;
