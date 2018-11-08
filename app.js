5aa31c553f815af97b928e456aab2af2109b9d1d
YANGBINA <1539068922@qq.com>
2018-11-08 13:38:55 +0800

    'asd'

--- app.js
+++ app.js
@@ -22,13 +22,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api/login/account', indexRouter);
 app.use('/users', usersRouter);
 
+app.use((req,res,next)=>{
+  res.sendFile(path.join(__dirname,'public/index.html'));
+})
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
-app.use((req,res,next)=>{
-  res.sendFile(path.join(__dirname,'public/index.html'));
-})
+
 
 // error handler
 app.use(function(err, req, res, next) {
